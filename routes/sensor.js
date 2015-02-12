10d74758ed3d6348df0fd0d8de4745c6cc83938b
goliatone <burgosemi@gmail.com>
2015-02-11 23:02:30 -0500

    ROUTE: sensor emit socket on update

--- routes/sensor.js
+++ routes/sensor.js
@@ -10,12 +10,16 @@ router.get('/', function(req, res) {
 //curl -H "Content-Type: application/json" -d '{"uuid":"xyz","t":"30", "h":"40"}' http://localhost:3000/sensor/collect
 router.post('/collect', function(req, res){
     console.log(req.body);
+    //THIS IS A JOKE :)
+    router.app.server.io.sockets.emit('update', req.body);
+
     res.send({
-        success:true
+        status: true
     });
 });
 
 
 module.exports = function(app) {
     app.use('/sensor', router);
+    router.app = app;
 };
