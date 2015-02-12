23a12cc1d76cdc22b6c5e94779f008cbcedc376b
goliatone <burgosemi@gmail.com>
2015-02-11 20:18:22 -0500

    APP: Use ejs

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var ejs = require('ejs');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -12,7 +14,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', ejs);
+app.engine('html', ejs.renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
