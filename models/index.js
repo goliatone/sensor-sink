d6e65ef2ff6faef90228600d3c26b6fe5c7d926a
goliatone <burgosemi@gmail.com>
2015-02-12 15:40:57 -0500

    MODEL: Added index, WIP

---
+++ models/index.js
@@ -0,0 +1,33 @@
+require('tungus');
+
+var restful = require('node-restful'),
+    path = require('path'),
+    mongoose = restful.mongoose;
+
+mongoose.connect('tingodb://' + __dirname + '/data');
+
+module.exports = function register(app){
+    console.log('REGISTER models')
+    var Device = app.Device = restful.model('device', mongoose.Schema({
+        uuid: 'string',
+        token:'string',
+        timestamp:  'number',
+    }))
+    .methods(['get', 'post', 'put', 'delete']);
+
+    Device.route('scan.get', function(req, res, next) {
+        res.send('DEVICE:I have a recommendation for you!');
+    });
+
+    Device.register(app, '/api/devices');
+};
+
+// var db = require('./../mongooseModels'),
+//     restful = require('../restful');
+
+// restful.addResource({
+//   app: app,
+//   urlPrefix: 'api',
+//   resourceName: 'users',
+//   collection: db.user
+// });
