08bd14a19f6706cbf0995e18acf008ab3e1a669b
goliatone <burgosemi@gmail.com>
2015-02-11 21:46:35 -0500

    APP: Added sensor route

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var ejs = require('ejs');
 
-//ROUTES
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+app.set('env', 'development');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,10 +24,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//ROUTES
+var routes = require('./routes/index');
 app.use('/', routes);
+
+var users = require('./routes/users');
 app.use('/users', users);
 
-
+require('./routes/sensor')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,7 +48,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render('error.html', {
             message: err.message,
             error: err
         });
@@ -56,7 +59,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('error.html', {
         message: err.message,
         error: {}
     });
