5de3bb872f4ba4dfa4cb29275d98368e21557d06
goliatone <burgosemi@gmail.com>
2015-02-15 23:20:20 -0500

    MODELS: Device wip

--- models/index.js
+++ models/index.js
@@ -12,7 +12,7 @@ module.exports = function register(app){
     //curl -H "Content-Type: application/json" -d '{"uuid":"xyz","token":"AD90ADF90ADF88ADF0ADF", "timestamp":1423761255422}' http://localhost:3000/api/devices
     var Device = app.Device = restful.model('device', mongoose.Schema({
         uuid: 'string',
-        token:'string',
+        // token:'string',
         // objectId: mongoose.Schema.ObjectId,
         timestamp:  'number',
     }))
