918beaf674eabd6911b25b1c0a563e45b9f7e2dd
goliatone <burgosemi@gmail.com>
2015-02-16 21:00:23 -0500

    MIDDLEWARE: sockets TODO: Add io to request with middleware

--- middleware/sockets.js
+++ middleware/sockets.js
@@ -2,10 +2,18 @@ var Pubsub = require('../lib/eventchannel'),
     io;
 
 
-module.exports = function register(server) {
+module.exports = function register(app, server) {
     io = require('socket.io').listen(server);
 
     server.io = io;
+
+    // Make sockets io accessible on each request
+    // TODO: We might have to do this before adding routes?
+    app.use(function(req,res,next){
+
+        req.io = io;
+        next();
+    });
 
     io.on('connection', function(socket) {
         console.log('REGISTER NEW SOCKET')
