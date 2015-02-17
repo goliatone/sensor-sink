e39f2839cea80313d4e1c90cac32f6e2c8c27c16
goliatone <burgosemi@gmail.com>
2015-02-17 17:27:04 -0500

    TEST: API test wip

--- tests/api_test.js
+++ tests/api_test.js
@@ -10,32 +10,32 @@ describe('express rest API for devices', function() {
         superagent.post('http://localhost:3000/api/devices')
             .send({
                 uuid: UUID,
-                type: 'test-device'
+                _type: 1
             })
             .end(function(e, res) {
-                console.log(res.body)
-                console.log('============')
+                // console.log(res.body)
+                // console.log('============')
                 expect(e).to.eql(null);
-                expect(res.body.length).to.eql(1);
-                expect(res.body[0]._id.length).to.eql(24);
-                id = res.body[0]._id;
+                expect(typeof res.body).to.eql('object');
+                expect(res.body._id);
+                id = res.body._id;
                 done();
             });
     });
 
-    xit('retrieves an object', function(done) {
+    it('retrieves an object', function(done) {
         superagent.get('http://localhost:3000/api/devices/' + id)
             .end(function(e, res) {
                 console.log(res.body)
                 expect(e).to.eql(null);
                 expect(typeof res.body).to.eql('object');
-                expect(res.body._id.length).to.eql(24);
+                expect(res.body._id);
                 expect(res.body._id).to.eql(id);
                 done();
             });
     });
 
-    xit('retrieves a collection', function(done) {
+    it('retrieves a collection', function(done) {
         superagent.get('http://localhost:3000/api/devices')
             .end(function(e, res) {
                 // console.log(res.body)
@@ -48,41 +48,39 @@ describe('express rest API for devices', function() {
             });
     });
 
-    xit('updates an object', function(done) {
+    it('updates an object', function(done) {
         superagent.put('http://localhost:3000/api/devices/' + id)
             .send({
-                uuid: 'adsfasdf3a21df3ad21fa3d21fa3d1',
-                type: 'test-device'
+                _type: 1
             })
             .end(function(e, res) {
                 // console.log(res.body)
                 expect(e).to.eql(null);
                 expect(typeof res.body).to.eql('object');
-                expect(res.body.msg).to.eql('success');
                 done();
             });
     });
 
-    xit('checks an updated object', function(done) {
+    it('checks an updated object', function(done) {
         superagent.get('http://localhost:3000/api/devices/' + id)
             .end(function(e, res) {
                 // console.log(res.body)
                 expect(e).to.eql(null);
                 expect(typeof res.body).to.eql('object');
-                expect(res.body._id.length).to.eql(24);
+                expect(res.body._id.length);
                 expect(res.body._id).to.eql(id);
-                expect(res.body.name).to.eql('Peter');
+                expect(res.body._type).to.eql(1);
                 done();
             });
     });
 
-    xit('removes an object', function(done) {
+    it('removes an object', function(done) {
         superagent.del('http://localhost:3000/api/devices/' + id)
             .end(function(e, res) {
                 // console.log(res.body)
                 expect(e).to.eql(null);
                 expect(typeof res.body).to.eql('object');
-                expect(res.body.msg).to.eql('success');
+                // expect(res.body.msg).to.eql('success');
                 done();
             });
     });
