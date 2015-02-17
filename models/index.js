7637e3116619e6d2d8fc3af1a3f18fb8784c1e8e
goliatone <burgosemi@gmail.com>
2015-02-16 21:00:45 -0500

    MODELS: index, WIP

--- models/index.js
+++ models/index.js
@@ -1,17 +1,36 @@
-require('tungus');
+// var restful = require('node-restful'),
+//     path = require('path'),
+//     mongoose = restful.mongoose;
 
-var restful = require('node-restful'),
-    path = require('path'),
-    mongoose = restful.mongoose;
+var express = require('express');
 
-mongoose.connect('tingodb://' + __dirname + '/data');
 
 module.exports = function register(app){
     console.log('REGISTER models');
 
+    var restful = require('../lib/restful');
+    var models = require('./device');
+
+    restful.addResource({
+        app: app,
+        urlPrefix: 'api',
+        resourceName: 'devices',
+        collection: models.Device
+    });
+
+    restful.addResource({
+        app: app,
+        urlPrefix: 'api',
+        resourceName: 'locations',
+        collection: models.Location
+    });
+
+    return
     //curl -H "Content-Type: application/json" -d '{"uuid":"xyz","token":"AD90ADF90ADF88ADF0ADF", "timestamp":1423761255422}' http://localhost:3000/api/devices
     var Device = app.Device = restful.model('device', mongoose.Schema({
         uuid: 'string',
+        type: 'string',
+
         // token:'string',
         // objectId: mongoose.Schema.ObjectId,
         timestamp:  'number',
@@ -24,6 +43,13 @@ module.exports = function register(app){
 
     Device.register(app, '/api/devices');
 };
+
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('SIGINT: Mongoose default connection disconnected.');
+        process.exit(0);
+    });
+});
 
 // var db = require('./../mongooseModels'),
 //     restful = require('../restful');
