3ef51aec68176027ed075e00a31de8775f637e5b
goliatone <burgosemi@gmail.com>
2015-02-18 14:27:17 -0500

    APP: refactoring app shell

--- app.js
+++ app.js
@@ -1,64 +1,21 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var ejs = require('ejs');
 
+//Configura DB
+require('./lib/setup/db')();
 
-///
-// Connect to mongodb
-require('tungus'); //mock mongodb, for now.
-var mongoose = require('mongoose');
-var connect = function () {
-    var options = { server: { socketOptions: { keepAlive: 1 } } };
-    // mongoose.connect(config.db, options);
-    var dbpath = 'tingodb://' + __dirname + '/models/data';
-    mongoose.connect(dbpath, options);
-};
-
-connect();
-
-mongoose.connection.on('error', console.log);
-mongoose.connection.on('disconnected', connect);
-
-
-process.on('SIGINT', function() {
-    mongoose.connection.close(function () {
-        console.log('SIGINT: Mongoose default connection disconnected.');
-        process.exit(0);
-    });
-});
-///
 
 var app = express();
 
-app.set('env', 'development');
+global.__root = __dirname;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', ejs);
-app.engine('html', ejs.renderFile);
-
-// uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+require('./lib/setup/server')(app, {
+    root:__dirname
+});
 
 
 //ROUTES
-var routes = require('./routes/index');
-app.use('/', routes);
-
-var users = require('./routes/users');
-app.use('/users', users);
-
+require('./routes/index')(app);
+require('./routes/users')(app);
 require('./routes/sensor')(app);
 
 //MODELS
