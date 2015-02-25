db18ca979de4e85010b4a7c1871a049856cff55f
goliatone <burgosemi@gmail.com>
2015-02-25 14:14:46 -0500

    fix merge..

--- models/device.js
+++ models/device.js
@@ -2,58 +2,49 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     Pubsub = require('../lib/eventchannel'),
-    URLSlugs = require('mongoose-url-slugs'),
-    timestamps = require('mongoose-timestamp'),
     uuidPplugin = require('../lib/mongoose-uuid'),
-    pubSubPlugin = require('../lib/mongoose-pubsub');
+    timestamps = require('mongoose-timestamp'),
+    URLSlugs = require('mongoose-url-slugs');
 
 var Device = new Schema({
-    uuid: {
-        type: String /*, required: true*/ ,
-        index: {
-            unique: true,
-            sparse: true
-        }
-    },
-    name: {
-        type: String,
-        trim: true
-    },
-    description: {
-        type: String,
-        trim: true
-    },
-    status: {
-        type: String,
-        trim: true
-    },
-    _type: {
-        type: Schema.ObjectId,
-        ref: 'DeviceType'
-    },
-    _location: {
-        type: Schema.ObjectId,
-        ref: 'Location'
-    }
-}, {
-    resourceName: 'device'
+    uuid        : { type: String, required: true, index: { unique: true, sparse: true } },
+    name        : { type: String, trim: true },
+    description : {type: String, trim: true},
+    status      : {type: String, trim: true},
+    _type       : { type: Schema.ObjectId, ref: 'DeviceType' },
+    _location   : { type: Schema.ObjectId, ref: 'Location' }
 });
 
+//////Event Types
+///TODO: Figure out how to catch validation errors!!!!
+var eventType;
+Device.pre('save', function (next) {
+    eventType = this.isNew ? 'create' : 'update';
 
+    this.on('error', function(){
+        Pubsub.emit('device.error', this);
+    }.bind(this));
+
+    next();
+});
+
+Device.post('validate', function (doc) {
+  console.log('%s has been validated (but not saved yet)', doc._id, doc);
+})
+
+Device.post('save', function(doc){
+    Pubsub.emit('device.'+eventType, this);
+    eventType = null;
+});
+
+Device.post('remove', function (doc) {
+    Pubsub.emit('device.delete', doc);
+});
+////////////
 
 Device.plugin(uuidPplugin);
-
 Device.plugin(timestamps);
-
-Device.plugin(pubSubPlugin, {
-    pubSub: function() {
-        return Pubsub;
-    }
-});
-
-Device.plugin(URLSlugs('name', {
-    field: 'slug'
-}));
+Device.plugin(URLSlugs('name', {field: 'slug'}));
 
 
 module.exports = mongoose.model('Device', Device);
