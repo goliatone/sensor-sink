437b7596ffeac1ddfd537f5cbb64865d8a82ac64
goliatone <burgosemi@gmail.com>
2015-02-25 23:37:03 -0500

    JS: Added app.js

---
+++ public/javascripts/app/app.js
@@ -0,0 +1,13 @@
+/*
+ * leadership_board
+ * Wall Exhibit: Leadership Board
+ *
+ */
+/*global define:true*/
+/* jshint strict: false */
+define('app', function(require) {
+    console.log('APP')
+    return function(){};
+
+
+});
