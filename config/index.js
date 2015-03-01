1c651efb66c3cabafe57e8556d468679e2321d31
goliatone <burgosemi@gmail.com>
2015-03-01 00:32:28 -0500

    CONFIG: WIP main handler

---
+++ config/index.js
@@ -0,0 +1,25 @@
+/**
+ * Module dependencies.
+ */
+
+var path = require('path');
+var extend = require('gextend');
+
+//TODO: Load all file in env directory,
+//each has a method to figure out if its the right one
+var production = require('./env/prod');
+var development = require('./env/dev');
+
+var DEFAULTS = {
+  root: path.normalize(__dirname + '/..')
+};
+
+/**
+ * Expose
+ */
+
+module.exports = {
+  dev: extend({}, DEFAULTS, development),
+  development: extend({}, DEFAULTS, development),
+  production: extend({}, DEFAULTS, production)
+}[process.env.NODE_ENV || 'development'];
