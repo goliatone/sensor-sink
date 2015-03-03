97f96f8866e4bfd6311de070ab0387f2a0c29c5e
goliatone <burgosemi@gmail.com>
2015-03-03 00:47:23 -0500

    WIDGET: WIP number widget

--- public/js/app/widgets/number/number.js
+++ public/js/app/widgets/number/number.js
@@ -1,10 +1,20 @@
 define('numberwidget', function(require){
     console.log('NUMBER WIDGET');
 
+    require('css!/js/app/widgets/number/number');
+
+    var template = require('text!/js/app/widgets/number/number.html');
+
+
     var Ractive = require('ractive');
     var NumberWidget = Ractive.extend({
-        template:'#numberwidget-template',
-        append:true
+        template: template,
+        append:true,
+        data:{
+            value: 23,
+            units: '˚C',
+            label: 'Temperature'
+        }
     });
 
     Ractive.components['number-widget'] = NumberWidget;
