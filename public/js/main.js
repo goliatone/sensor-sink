6d7a67754a6e99febb2265c7e91d80dc0209704c
goliatone <burgosemi@gmail.com>
2015-03-02 23:48:58 -0500

    JS: bootstrap app

--- public/js/main.js
+++ public/js/main.js
@@ -4,20 +4,26 @@ requirejs.config({
     baseUrl: 'js',
     paths: {
         'app': 'app/app',
-        // 'gpub': 'components/gpub/gpub',
+        'socket': 'app/services/socket',
+        'gpub': 'vendors/gpub/src/gpub',
+
+        'numberwidget': 'app/widgets/number/number',
+        //
         // 'gconfig': 'components/gconfig/gconfig',
         // 'gconfig.path': 'components/gconfig/gconfig.path',
         // 'gconfig.qstring': 'components/gconfig/gconfig.qstring',
         // 'gconfig.interpolate': 'components/gconfig/gconfig.interpolate',
 
-        // 'extend': 'components/gextend/extend',
+        'io': '/socket.io/socket.io.js',
+
+        'extend': 'vendors/gextend/src/extend',
         // 'keypath': 'components/gkeypath/keypath',
         // 'templatecontext': 'components/templatecontext/templatecontext',
 
 
 
         // 'preloader': 'views/preloader',
-        // 'ractive': 'components/ractive/ractive',
+        'ractive': 'vendors/ractive/ractive',
         // 'jquery': 'components/jquery/jquery',
 
     }
@@ -25,6 +31,27 @@ requirejs.config({
 define('main', function(require) {
     console.log('Loading');
 
+    var Client = require('socket');
+    var socket = new Client();
+
     var App = require('app');
+
+    var app = new App();
+
+    var NumberWidget = require('numberwidget');
+
+    var ractive = require('ractive');
+
+    var view = new Ractive({
+        el:'content',
+        // append:true,
+        template:'#content-template',
+        data:{
+            name:'Pepe',
+            adjective:'awesome'
+        }
+    });
+
+    window.view = view;
 
 });
