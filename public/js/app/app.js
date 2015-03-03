339a4f8e753d869cf4fd2704e28a1708ebcfb744
goliatone <burgosemi@gmail.com>
2015-03-02 23:48:42 -0500

    JS: wip making widgets

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -6,8 +6,30 @@
 /*global define:true*/
 /* jshint strict: false */
 define('app', function(require) {
-    console.log('APP')
-    return function(){};
 
+    var extend = require('extend');
+
+    var DEFAULTS = {
+        autoinitialize: true
+    };
+
+    var App = function(config){
+        config = extend({}, this.constructor.DEFAULTS, config);
+        if(config.autoinitialize ) this.init(config);
+    };
+
+    App.DEFAULTS = DEFAULTS;
+
+    App.prototype.init = function(config){
+        if(this.initialized) return;
+        this.initialized = true;
+        this.logger.info('App instance created');
+        extend(this, this.constructor.DEFAULTS, config);
+
+    };
+
+    App.prototype.logger = console;
+
+    return App;
 
 });
