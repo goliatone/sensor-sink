9567d0d75cbcbb5961b8a9e0b2b183b104b14cd9
goliatone <burgosemi@gmail.com>
2015-03-04 00:11:09 -0500

    WIDGET: number.js register multiple instances.

--- public/js/app/widgets/number/number.js
+++ public/js/app/widgets/number/number.js
@@ -5,17 +5,26 @@ define('numberwidget', function(require){
 
     var template = require('text!/js/app/widgets/number/number.html');
 
-
+    //TODO: State feedback. Reading...
     var Ractive = require('ractive');
+
     var NumberWidget = Ractive.extend({
         template: template,
         append:true,
-        data:{
-            value: 23,
+        init:function(o){
+
+        },
+        data: {
+            type:'widget',
+            value: '-',
             units: '˚C',
             label: 'Temperature'
         }
     });
+
+    NumberWidget.register = function(id){
+        Ractive.components[id] = NumberWidget;
+    };
 
     Ractive.components['number-widget'] = NumberWidget;
 
