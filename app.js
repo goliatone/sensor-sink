39a727cd156566273e97e6ba9f2131220d59dc92
goliatone <burgosemi@gmail.com>
2015-03-31 17:50:18 -0400

    APP: Added route to device

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.on('app.pre', function(payload){
 require('./routes/index')(app);
 require('./routes/users')(app);
 require('./routes/sensor')(app);
+require('./routes/device')(app);
 
 require('./controllers')(app, config);
 
