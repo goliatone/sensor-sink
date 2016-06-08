dece35ebec7d021b809d5cf06ca4b6d9831f9d25
goliatone <burgosemi@gmail.com>
2016-06-08 16:28:46 -0400

    JS: main, adding sound/lux transformations for now

--- public/js/main.js
+++ public/js/main.js
@@ -64,7 +64,11 @@ define('main', function(require) {
     var view = new Ractive({
         el: 'content',
         debug: true,
-        template: '#content-template'
+        template: '#content-template',
+        data:{
+            location: 'R&D',
+            title: 'Phonebooth Dashboard'
+        }
     });
 
     socket.client.on('device.create', function(device){
@@ -78,11 +82,11 @@ define('main', function(require) {
     //TODO: Get from configuration file!
     var WIDGETS = [
         {id:'temperature-widget', keypath:'payload.*.t'},
-        {id: 'light-widget', keypath:'payload.*.l'},
+        {id: 'light-widget', keypath:'payload.*.l', options:{transform: {func:luxTransformation, ops:{resistance:10}}}},
         {id: 'humidity-widget', keypath: 'payload.*.h'},
         {id: 'humidity-sparkle', keypath: 'payload.*.h', options:{label:'Humidity'}},
         {id: 'temperature-sparkle', keypath: 'payload.*.t', options:{label:'Temperature'}},
-        {id: 'sound-widget', keypath: 'payload.*.s'},
+        {id: 'sound-widget', keypath: 'payload.*.s', options:{transform:{func: soundTransformation, ops:{gain: 0}}}},
         {id: 'movement-widget', keypath: 'payload.*.m'}
     ];
 
@@ -94,3 +98,29 @@ define('main', function(require) {
     window.view = view;
 
 });
+
+function luxTransformation(value, ops){
+    if(!value) return 0;
+    if(value === 0) return value;
+    //http://forum.arduino.cc/index.php?topic=37555.0
+    // http://www.seeedstudio.com/wiki/Grove_-_Light_Sensor
+    var res0 = ops.resistance || 10;
+    var vout = value * (5/1024);//5V gets split up into 1024 levels
+    var lux = 500 / (res0 * (5 - vout)/vout);
+    return lux.toFixed(ops.decimals || 2);
+}
+
+function soundTransformation(value, ops){
+    if(!value) return 0;
+    if(value === 0) return value;
+    //http://www.seeedstudio.com/wiki/Grove_-_Sound_Sensor
+    //http://www.seeedstudio.com/wiki/Grove_-_Loudness_Sensor
+    //http://electronics.stackexchange.com/questions/96205/how-to-convert-volts-in-db-spl
+    var v1 = value * (5/1024);//5V gets split up into 1024 levels
+    var v0 = 0.005012; //ref voltage -46dBV/Pa assuming 1Pa
+    var x = Math.log10(v1/v0) * 20; //sensitivity of -46dBV/Pa , this gives 0.005012 V RMS/ Pa
+    var gain = ops.gain || 0;
+    var db = (-46) + x + 94 - gain;
+    if(isNaN(db)) return 0;
+    return db.toFixed(ops.decimals || 2);
+}
