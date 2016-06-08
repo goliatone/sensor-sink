40e3f3397af657de2fbcd97709ef5725aea42774
goliatone <burgosemi@gmail.com>
2016-06-08 14:37:29 -0400

    config: remove treehugger, use envset

--- config/index.js
+++ config/index.js
@@ -4,11 +4,6 @@
 
 var path = require('path');
 var extend = require('gextend');
-var TreeHugger = require('node-treehugger');
-
-TreeHugger({
-    namespace:'NODE'
-}).fromFile(__dirname + '/.secrets.json');
 
 //TODO: Load all file in env directory,
 //each has a method to figure out if its the right one
